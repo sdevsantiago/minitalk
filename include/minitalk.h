1eb148c7c4ee4fb92abc43ea20222af1fb11c96c
sede-san <sede-san@student.42madrid.com>
2025-04-15 16:02:54 +0200

    update: added new include to avoid VScode Intellisense error

--- include/minitalk.h
+++ include/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sede-san <sede-san@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:44:01 by sede-san          #+#    #+#             */
-/*   Updated: 2025/04/14 17:34:20 by sede-san         ###   ########.fr       */
+/*   Updated: 2025/04/15 15:31:50 by sede-san         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include "../lib/Libft/libft.h"
 # include <signal.h>
+# include <bits/sigaction.h>
 
 # define SIGSTARTTX SIGUSR1
 # define SIGENDTX SIGUSR2
